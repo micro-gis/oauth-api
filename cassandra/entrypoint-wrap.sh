127e59352435453a08284d93725fec986e2f5fbb
yossefazoulay <azoulayos@protonmail.com>
2020-10-14 16:16:44 +0300

    add delete route for delete all user tokens

--- cassandra/entrypoint-wrap.sh
+++ cassandra/entrypoint-wrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2089
-  CQL="CREATE KEYSPACE IF NOT EXISTS oauth WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};USE oauth;CREATE TABLE IF NOT EXISTS access_tokens( access_token varchar PRIMARY KEY, user_id bigint, client_id bigint, expires bigint);"
+  CQL="CREATE KEYSPACE IF NOT EXISTS oauth WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};USE oauth;CREATE TABLE IF NOT EXISTS access_tokens( access_token varchar PRIMARY KEY, user_id bigint, client_id bigint, expires bigint);create index access_tokens_user_id_index on access_tokens (user_id);"
   until echo $CQL | cqlsh cassandra-main; do
     echo "cqlsh: Cassandra is unavailable - retry later"
     sleep 2
